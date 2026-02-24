87be9089d19c8f3e74417a84c8625e6e33c92051
Thronetaker <parmanand224radha@gmail.com>
2026-02-25 00:01:11 +0530

    Rod Cutting

---
+++ dp/Rod Cutting.java
@@ -0,0 +1,38 @@
+public class Classroom {
+
+    // weight => length; val => price; W => totRod
+    public static int rodCutting(int length[], int price[], int totRod) {
+
+        int n = price.length;
+        int dp[][] = new int[n + 1][totRod + 1];
+
+        // initialize
+        for (int i = 0; i < n + 1; i++) {
+            for (int j = 0; j < totRod + 1; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+
+        // DP computation
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < totRod + 1; j++) {
+
+                // valid
+                if (length[i - 1] <= j) {
+                    dp[i][j] = Math.max(
+                        price[i - 1] + dp[i][j - length[i - 1]],
+                        dp[i - 1][j]
+                    );
+                }
+                // invalid
+                else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+
+        return dp[n][totRod];
+    }
+}
