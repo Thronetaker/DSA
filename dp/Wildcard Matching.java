edbc8c8f009defca4d4ccf286169808b4d6689b7
Thronetaker <parmanand224radha@gmail.com>
2026-02-24 23:57:44 +0530

    DP

---
+++ dp/Wildcard Matching.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int n = s.length();
+        int m = p.length();
+
+        boolean[][] dp = new boolean[m+1][n+1];
+        dp[0][0] = true;
+        for(int i=1; i<=m; i++){
+            dp[i][0] = (p.charAt(i-1) == '*') && dp[i-1][0];
+        }
+        for(int i=1; i<=m; i++){
+            for(int j=1; j<=n; j++){
+                if(p.charAt(i-1) == s.charAt(j-1) || p.charAt(i-1) == '?'){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else if(p.charAt(i-1) == '*'){
+                    dp[i][j] = dp[i-1][j] || dp[i][j-1];
+                }
+                else{
+                    dp[i][j] = false;
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
