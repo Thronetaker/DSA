415948f5cdb0069d4627f36100dcb00dbe5acea4
Thronetaker <parmanand224radha@gmail.com>
2026-06-17 23:38:33 +0530

    CatalanRecursion

---
+++ dp/catalanNumber.java
@@ -0,0 +1,16 @@
+public class catalanNumber {
+    
+    public static int catalanRec(int n){
+        if(n==0 || n == 1) return 1;
+
+        int ans = 0 ;
+        for(int i = 0 ; i < n ; i++){
+            ans += catalanRec(i) * catalanRec(n-i-1);
+        }
+        return ans ;
+    }
+    public static void main( String args[]){
+        int n = 4 ;
+        System.out.println(catalanRec(n));
+    }
+}
