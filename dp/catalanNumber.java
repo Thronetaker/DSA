142f3be2b68aac3023b5a9b79432faca953d10ac
Thronetaker <parmanand224radha@gmail.com>
2026-06-17 23:54:57 +0530

    Catalan Tabulation

--- dp/catalanNumber.java
+++ dp/catalanNumber.java
@@ -1,5 +1,18 @@
 import java.util.*;
 public class catalanNumber {
+    //table, meaing +init, fill dp bottom up
+    public static int catalanTabulation(int n){ //O(n^2)
+        int dp[] = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        
+        for(int i = 2 ; i < n ; i++){ //Ci
+            for(int j =0 ; j< i; j++){
+                dp[i] += dp[j] * dp[i-j-1];
+            }
+        }
+        return dp[n] ;
+    }
     public static int catalanMemoization(int n, int dp[]){
         if(n==0 || n == 1) return 1;
         
