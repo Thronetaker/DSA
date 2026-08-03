3592562d1a1cea96e45e9b57d7d3eada4ee08072
Thronetaker <parmanand224radha@gmail.com>
2026-08-03 23:17:26 +0530

    modifications

--- Cwise/strive/cntSumK.java
+++ Cwise/strive/cntSumK.java
@@ -4,18 +4,17 @@ class cntSumK{
 
     public int cntWays(int arr[], int target){
         int n = arr.length;
-        int dp[][] = new int[n][target+1];
+        int dp[][] = new int[n+1][target+1];
 
         for(int i = 0 ;i<n ;i++) dp[i][0] = 1;
 
-        if( arr[0] <= target) dp[0][arr[0]] = 1;
 
         for(int i = 1; i<n ; i++){
             for(int j = 1; j<=target ; j++){
 
                 int notTaken = dp[i-1][j];
                 int taken = 0;
-                if( arr[i] <= j) taken = dp[i-1][j-arr[i]];
+                if( arr[i -1] <= j) taken = dp[i-1][j-arr[i -1]];
 
                 dp[i][j] = taken + notTaken ;
             }
